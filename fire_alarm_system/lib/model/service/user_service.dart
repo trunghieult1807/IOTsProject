4dd366ee99fb1a9aa375c93ca6ee6c101bd501ac
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2021-05-09 22:48:36 +0700

    Add streaming to user_service

--- fire_alarm_system/lib/model/service/user_service.dart
+++ fire_alarm_system/lib/model/service/user_service.dart
@@ -12,7 +12,7 @@ class UserService{
 
   static void setFireThreshold(int newThreshold){
     String uid = FirebaseAuth.instance.currentUser.uid;
-    db.collection('users').doc(uid).set({'fireThreshold': newThreshold});
+    db.collection('users').doc(uid).update({'fireThreshold': newThreshold});
   }
 
   static Future<int> getWarningThreshold() async{
@@ -22,7 +22,15 @@ class UserService{
 
   static void setWarningThreshold(int newThreshold){
     String uid = FirebaseAuth.instance.currentUser.uid;
-    db.collection('users').doc(uid).set({'warningThreshold': newThreshold});
+    db.collection('users').doc(uid).update({'warningThreshold': newThreshold});
   }
 
+  static Stream<int> getFireThresholdStream(){
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    return db.collection('users').doc(uid).snapshots().map((snapshot) => snapshot['fireThreshold']);
+  }
+  static Stream<int> getWarningThresholdStream(){
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    return db.collection('users').doc(uid).snapshots().map((snapshot) => snapshot['warningThreshold']);
+  }
 }
