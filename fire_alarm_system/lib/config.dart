9108009e3bb87c61c7c04d0be2824d546613492b
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2021-05-31 10:25:34 +0700

    Revert "Delete config.dart"
    This reverts commit b85393e5021a138b405ac164094aee15732e9a5e.

---
+++ fire_alarm_system/lib/config.dart
@@ -0,0 +1,25 @@
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+class Config {
+  static MqttServerClient
+      gasSensorClient; // = mqttsetup.setup('io.adafruit.com', 1833);
+  static MqttServerClient tempSensorClient;
+  static MqttServerClient ledClient;
+  static MqttServerClient buzzerClient;
+  static MqttServerClient relayClient;
+
+  static String username = '2TMH4';
+  //static String username = 'test'; // If username is 'test', we will switch to the two test feed (used by teacher)
+  static String apikey = 'aio_YjHf96tHbBcazR57pAqnnnznlbPr';
+
+  static String testName0 = 'CSE_BBC';
+  static String testKey0 = 'aio_atdS67wAR8mAG3s0MfgeS6ydx7fi';
+
+  static String testName1 = 'CSE_BBC1';
+  static String testKey1 = 'aio_Hxwq840PwJEgoJ65gkVwokjEsfQ4';
+}
+
+class Global{
+  static int fireThreshold = 0;
+  static int warnThreshold = 0;
+}
