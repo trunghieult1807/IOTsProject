2d87d5f6a490edb3a1030d03a6185f1a982b5057
trunghieult1807 <trunghieult1807@gmail.com>
2021-05-31 10:59:21 +0700

    test feed

--- fire_alarm_system/lib/config.dart
+++ fire_alarm_system/lib/config.dart
@@ -8,9 +8,11 @@ class Config {
   static MqttServerClient buzzerClient;
   static MqttServerClient relayClient;
 
-  static String username = '2TMH4';
+  // static String username = '2TMH4';
   //static String username = 'test'; // If username is 'test', we will switch to the two test feed (used by teacher)
-  static String apikey = 'aio_GKXm07wvEPmhyQJBInqAIh5ku07J';
+  // static String apikey = 'aio_GKXm07wvEPmhyQJBInqAIh5ku07J';
+  static String username = 'haily835';
+  static String apikey = 'aio_IFZu2517SGPLkd41apSTgofxrM1d';
 
   static String testName0 = 'CSE_BBC';
   static String testKey0 = 'aio_atdS67wAR8mAG3s0MfgeS6ydx7fi';
